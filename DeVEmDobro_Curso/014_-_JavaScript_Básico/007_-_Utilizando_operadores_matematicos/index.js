1323461b18a060c76a5020f64e9a098526f62af9
Marcelo F. Duarte <mfdzerohour@hotmail.com>
2024-04-11 16:09:14 -0400

    Operações matematicas basicas em JavaScript, soma(+), subtração(-), divisao(/), multiplicacao(*) e resto(%)

---
+++ DeVEmDobro_Curso/014_-_JavaScript_Básico/007_-_Utilizando_operadores_matematicos/index.js
@@ -0,0 +1,22 @@
+let primeiroValor = 29;
+let segundoValor = 3;
+
+let resultadoSoma = primeiroValor + segundoValor;
+
+console.log(resultadoSoma);
+
+let resultadoSubtracao = primeiroValor - segundoValor;
+
+console.log(resultadoSubtracao);
+
+let resultadoDivisao = primeiroValor / segundoValor;
+
+console.log(resultadoDivisao);
+
+let resultadoMultiplicacao = primeiroValor * segundoValor;
+
+console.log(resultadoMultiplicacao);
+
+let resultadoResto = primeiroValor % segundoValor;
+
+console.log(resultadoResto);

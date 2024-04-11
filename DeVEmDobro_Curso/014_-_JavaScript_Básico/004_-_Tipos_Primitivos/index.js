c80c9f6e23c6296d4de5894b546410cba5a3fbdf
Marcelo F. Duarte <mfdzerohour@hotmail.com>
2024-04-11 13:25:17 -0400

    Tipos de dados primitivos (string, number, boolean, null, undefined)

---
+++ DeVEmDobro_Curso/014_-_JavaScript_Básico/004_-_Tipos_Primitivos/index.js
@@ -0,0 +1,14 @@
+//Tipo String
+let nome = "Marcelo";
+
+//Tipo Number
+let number = 0.6666666;
+
+//Tipo Boolean
+let usuarioVerificado = false;
+
+//Tipo Null
+let pessoa = null;
+
+//Tipo Undefined
+let telefone = undefined

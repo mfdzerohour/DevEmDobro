f4620aa6c849274ce93f0fe527f8dbe77d991b1b
Marcelo F. Duarte <mfdzerohour@hotmail.com>
2024-04-12 09:16:17 -0400

    Para o laço de repetição for, só alterado o nome da pasta principal, para saber qual é o laço de repetição naquela pasta.

---
+++ DeVEmDobro_Curso/014_-_JavaScript_Básico/009_-_O_que_sao_e_para_que_servem_os_lacos_de_repeticao_-_WHILE_-_parte2/index.js
@@ -0,0 +1,30 @@
+/*
+    WHILE - Como usar - o teste de condição é no inicio
+
+    while(condicao){
+        <Faça isto>
+    }
+*/
+
+// let a = 10
+// let b = 15
+
+// while (a < b) {
+//     console.log("Incrementando a variável 'a' => " + a);
+//     a++;
+// }
+
+/*
+    DO
+    WHILE - Como usar - o teste de condição é no fim
+
+    DO{
+        <bloco de execução>
+    } WHILE (condicao)
+*/
+
+var i = 1
+do {
+    console.log("Entrou: i = " + i);
+    i++;
+}while( i <= 5)

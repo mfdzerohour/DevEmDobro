33fee912fecfae9487e9d78fcb8f6c8db9d3adb9
Marcelo F. Duarte <mfdzerohour@hotmail.com>
2024-05-03 15:46:18 -0400

    Escopo em JavaScript

---
+++ DeVEmDobro_Curso/016_-_JavaScript_Intermediario/001_-_Escopo_no_JavaScript/index.js
@@ -0,0 +1,38 @@
+// if (true) {
+//     const mensagem='ola'
+//     alert(mensagem)
+// }
+
+// alert(mensagem)
+
+// {
+//     var mensagem='ola'
+//     alert(mensagem)
+// }
+
+// alert(mensagem)
+
+// function falar(){
+//     var mensagem='Olá mundo!'
+//     alert(mensagem)
+// }
+
+// falar()
+// alert(falar)
+
+// function falar(){
+//     var mensagem='Olá mundo!'
+//     alert(mensagem)
+
+//     function falarOutraCoisa(){
+
+//     }
+//     falarOutraCoisa()
+//     console.log(falarOutraCoisa)
+// }
+
+// falar()
+// // alert(mensagem)
+// alert(falarOutraCoisa)
+
+alert(url)

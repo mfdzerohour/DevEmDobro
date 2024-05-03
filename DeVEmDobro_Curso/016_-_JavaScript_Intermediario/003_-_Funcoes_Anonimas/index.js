ed67a4bdc5f71bb471d57be10a3618bf1b676a72
Marcelo F. Duarte <mfdzerohour@hotmail.com>
2024-05-03 16:12:53 -0400

    Funcoes anonimas

---
+++ DeVEmDobro_Curso/016_-_JavaScript_Intermediario/003_-_Funcoes_Anonimas/index.js
@@ -0,0 +1,11 @@
+// function somar(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = somar(12, 5)
+// alert(resultadoDaSoma)
+
+let resultadoDaSoma = function(numero1, numero2){
+    return numero1 + numero2
+}
+alert(resultadoDaSoma(12, 5))

7304d081f4b48ab0f9e70b062fc2d54a6a3efc45
Marcelo F. Duarte <mfdzerohour@hotmail.com>
2024-05-06 16:41:46 -0400

    Arrays

---
+++ DeVEmDobro_Curso/016_-_JavaScript_Intermediario/006_-_Arrays/index.js
@@ -0,0 +1,8 @@
+// let filmes = ['Vingadores ', 'Mulher Maravilha ']
+// alert(filmes)
+
+// let descricaoFilmes = ['Vingadores',4.5,true]
+// alert(descricaoFilmes)
+
+let tecnologia = ['wifi', 'lan', 'optico']
+alert(tecnologia[2])

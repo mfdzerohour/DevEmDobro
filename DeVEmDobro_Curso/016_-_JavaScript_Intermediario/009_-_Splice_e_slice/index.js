a62399aa4ef26112e6bd3c6e47c40134e73460cc
mfdzerohour <mfdzerohour@hotmail.com>
2024-07-24 17:18:55 -0400

    Adicionando exemplos de slice splice, com exemplos

--- DeVEmDobro_Curso/016_-_JavaScript_Intermediario/009_-_Splice_e_slice/index.js
+++ DeVEmDobro_Curso/016_-_JavaScript_Intermediario/009_-_Splice_e_slice/index.js
@@ -1,24 +1,26 @@
+//O slice não remove o item do array original
+// let frutas = ['Banana', 'Maça', 'Laranja', 'Uva']
+
+//let frutasExtraidas = frutas.slice(1,3) //Corta da posição 1 até antes 3
+
+// let frutasExtraidas = frutas.slice(1) //Corta a partir da posição 1
+
+// alert(frutasExtraidas)
+//alert(frutas)
+
+
+
+//O splice remove o item do array original
+// let frutas = ['Banana', 'Maça', 'Laranja', 'Uva']
+
+// let frutasExtraidas = frutas.splice(1, 2)
+
+// console.log(frutasExtraidas)
+// console.log(frutas)
+
+//remove e adiciona o item
 let frutas = ['Banana', 'Maça', 'Laranja', 'Uva']
-
-// let frutasExtraidas = frutas.slice(1)
-
-// console.log(frutasExtraidas)
-
-// console.log(frutas)
-
-// let frutasExtraidas = frutas.splice(1,2)
-
-// console.log(frutasExtraidas)
-// console.log(frutas)
-
-let removeEAdicionaFrutas = frutas.splice(1, 2, "Goiaba", "Abacaxi", "Pêra")
+let removeEAdicionaFrutas = frutas.splice(1, 2, 'Goiaba', 'Abacaxi', 'Perâ')
 
 console.log(frutas)
-
-[
-    "Banana",
-    "Goiaba",
-    "Abacaxi",
-    "Pêra",
-    "Uva"
-]
+console.log(removeEAdicionaFrutas)

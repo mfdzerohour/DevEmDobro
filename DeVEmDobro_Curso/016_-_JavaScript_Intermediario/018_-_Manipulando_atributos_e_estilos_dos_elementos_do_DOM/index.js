08b9bbb408412009330f25689ae1277724ee467a
mfdzerohour <mfdzerohour@hotmail.com>
2024-07-26 17:56:31 -0400

    Manipulando atributos e estilos dos elementos do DOM

---
+++ DeVEmDobro_Curso/016_-_JavaScript_Intermediario/018_-_Manipulando_atributos_e_estilos_dos_elementos_do_DOM/index.js
@@ -0,0 +1,27 @@
+function alterarCorDeFundoPrimeiroPost(){
+    let posts = document.getElementsByClassName("post");
+
+    let primeiroPost = posts[0];
+
+    primeiroPost.style.backgroundColor = 'red';
+}
+
+function aumentarFonteSegundoPost(){
+    let textoPosts = document.getElementsByClassName("texto-post");
+
+    console.log(textoPosts[1].classList)
+
+    textoPosts[1].classList.add('font-grande');
+}
+
+function marcarRadio(genero){
+    console.log(genero)
+    let radioFeminino = document.getElementById('genero-feminino');
+    let radioMasculino = document.getElementById('genero-masculino');
+    
+    if(genero === 'F'){
+        radioFeminino.ariaChecked;
+    }
+}
+
+//14:44

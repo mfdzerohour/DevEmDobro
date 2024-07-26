249a59c141e45d9877f88e6d5ff80d2b8d20fabe
mfdzerohour <mfdzerohour@hotmail.com>
2024-07-26 12:51:42 -0400

    Diferenças entre objetos e arrays

---
+++ DeVEmDobro_Curso/016_-_JavaScript_Intermediario/013_-_Diferencas_entre_objetos_e_arrays/index.js
@@ -0,0 +1,22 @@
+// Array
+let frutas = ['uva', 'banana', 'melao']
+
+// Objeto
+let videoGame1 = {
+    nome: 'x-box',
+    valor: 3000
+}
+
+videoGame2 = {
+    nome: 'Playstation',
+    valor: 5000
+}
+
+//colocando objetos dentro de uma array
+let videoGames = [videoGame1, videoGame2]
+
+console.log(videoGames)
+
+console.log(videoGames[1])
+
+console.log(videoGames[1].nome)

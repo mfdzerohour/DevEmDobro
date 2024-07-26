c51103f81202302637d39a539728acdf8439c881
mfdzerohour <mfdzerohour@hotmail.com>
2024-07-26 16:21:02 -0400

    Usando o document para selecionar elementos do DOM

---
+++ DeVEmDobro_Curso/016_-_JavaScript_Intermediario/016_-_Usando_o_document_para_selecionar_elementos_do_DOM/index.js
@@ -0,0 +1,18 @@
+// // console.log(document)
+
+// let p = document.getElementById('paragrafo1')
+
+// console.log(p)
+
+// let inputsPorClasse = document.getElementsByClassName("tag-de-input")
+
+// console.log(inputsPorClasse)
+
+// let inputPorTag = document.getElementsByTagName('h1')
+
+// console.log(inputPorTag)
+
+
+let inputPorName = document.querySelectorAll("input[name='email']")
+
+console.log(inputPorName[1])

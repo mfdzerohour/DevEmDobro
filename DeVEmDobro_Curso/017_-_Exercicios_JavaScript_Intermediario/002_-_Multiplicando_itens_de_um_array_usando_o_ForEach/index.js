8324dd4a4a32b62491766539c4e2edea4e536fb4
mfdzerohour <mfdzerohour@hotmail.com>
2024-07-29 15:15:00 -0400

    Multiplicando itens de um array usando o ForEach, funcionando no console.log, mas não funcionando no HTML, ver como corrigir

---
+++ DeVEmDobro_Curso/017_-_Exercicios_JavaScript_Intermediario/002_-_Multiplicando_itens_de_um_array_usando_o_ForEach/index.js
@@ -0,0 +1,35 @@
+/*
+		Dica: Começar pensando no objetivo do que precisa fazer e no passo a passo pra chegar lá é essencial.
+    
+    NÂO SAIA TENTANDO ESCREVER CÓDIGO! Antes disso você precisa saber O QUE precisa ser feito e só depois COMO fazer.
+
+		Objetivo do exercício: Multiplicar itens de uma array por 10.
+
+		Passo 1 - Criar a array de itens.
+		
+		Passo 2 - Utilizar essa array declarada para utilizar o laço forEach.
+
+		Passo 3 - No forEach, utilizamos uma variável para iterar cada elemento da array.
+
+		Passo 4 - Imprimir os elementos já multiplicados por 10, utilizando o console.log().
+*/
+
+// *------------------------------------------------------------------------ *
+
+// Passo 1 - Criar a array de numeros
+const numeros = [65, 44, 12, 4];
+
+// Passo 2 - Utilizar essa array declarada para utilizar o laço ForEach.  
+let resultadoFinal = ""; // Variável para armazenar o resultado final
+
+numeros.forEach(
+				(item) => {
+					// Passo 3 - No  ForEach, utlizamos uma variável para iterar cada elemento da array.
+					let resultado = item * 10;
+					//console.log(item * 10);
+					// Passo 4 - Concatenar os resultados em uma única string
+					resultadoFinal += item + " * 10 = " + resultado + "<br>";
+					console.log(item, ' * 10 = ',  resultado = item * 10);
+					//document.getElementById("multiplicacao").innerHTML += item, ' * 10 = ',  resultado = item * 10;
+				}
+);

bd929e1ff6de35ec42ee8c5842c3a7e3b773a25f
mfdzerohour <mfdzerohour@hotmail.com>
2024-07-29 09:22:55 -0400

    Encontrando e interpretando erros no JS

---
+++ DeVEmDobro_Curso/016_-_JavaScript_Intermediario/020_-_Encontrando_e_interpretando_erros_no_JS/index.js
@@ -0,0 +1,15 @@
+// let valorBoleto = 100;
+
+// console.log(valorBoleto);
+
+// console.log(valorBoleto);
+
+let inputTelefone = document.getElementById("telefone");
+
+console.log(inputTelefone);
+
+inputTelefone.disabled = true;
+
+function habilitarTelefone(){
+    //Só de ter o nome da função mesmo vazia ele não exibe mais os erros ao clicar no botão
+}

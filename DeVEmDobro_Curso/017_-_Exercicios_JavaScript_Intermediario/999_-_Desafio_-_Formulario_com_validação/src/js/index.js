b85e285cff72bda1ea8e0b5ddf592453f8da4c27
mfdzerohour <mfdzerohour@hotmail.com>
2024-08-15 10:34:49 -0400

    Ajuste de informação de campo obrigatorio no CSS, ainda falta centralizar ele na vertical

--- DeVEmDobro_Curso/017_-_Exercicios_JavaScript_Intermediario/999_-_Desafio_-_Formulario_com_validação/src/js/index.js
+++ DeVEmDobro_Curso/017_-_Exercicios_JavaScript_Intermediario/999_-_Desafio_-_Formulario_com_validação/src/js/index.js
@@ -1,13 +1,24 @@
 const btnEnviar = document.getElementById('btn');
 const preenchido = document.getElementsByClassName("formularioPreenchimento");
 
-// Iterar sobre cada elemento preenchido
-Array.from(preenchidos).forEach(preenchido => {
-    btnEnviar.addEventListener("click", function() {
-        if (preenchido.value.trim() !== '') {
-            preenchido.classList.add('preenchido');
-        } else {
-            preenchido.classList.remove('preenchido');
-        }
-    });
-});
+function verificarCampoPreenchido(){
+    if (preenchido.value.trim() !== '') {
+        preenchido.classList.add('preenchido');
+    } else {
+        preenchido.classList.remove('preenchido');
+    }
+}
+
+
+
+// // Iterar sobre cada elemento preenchido
+// Array.from(preenchidos).forEach(preenchido => {
+//     btnEnviar.addEventListener("click", function() {
+//         alert("Funcionou!");
+//         // if (preenchido.value.trim() !== '') {
+//         //     preenchido.classList.add('preenchido');
+//         // } else {
+//         //     preenchido.classList.remove('preenchido');
+//         // }
+//     });
+// });

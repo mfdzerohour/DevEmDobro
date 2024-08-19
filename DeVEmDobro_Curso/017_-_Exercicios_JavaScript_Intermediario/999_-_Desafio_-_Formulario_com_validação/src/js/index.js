7c2c186e16eee31174dac6829fb2b58f6b18dc18
mfdzerohour <mfdzerohour@hotmail.com>
2024-08-19 12:48:45 -0400

    Ajuste de campos que serão exibidos aumentando o tamanho dele, ainda necessita de ajustes para não subir o tamanho indefinidamente

--- DeVEmDobro_Curso/017_-_Exercicios_JavaScript_Intermediario/999_-_Desafio_-_Formulario_com_validação/src/js/index.js
+++ DeVEmDobro_Curso/017_-_Exercicios_JavaScript_Intermediario/999_-_Desafio_-_Formulario_com_validação/src/js/index.js
@@ -1,8 +1,11 @@
-const campos = document.querySelectorAll('.campoFormulario');
+const campos = document.querySelectorAll('.campo-Formulario');
 const enviar = document.getElementById('btn');
+const formularioSection = document.querySelector('.formulario');
+// let totalHeight = 0;
 
-enviar.addEventListener('click', function(){
+enviar.addEventListener('click', function() {
     verificarPreenchimento();
+    ajustarTamanhoForm();
 });
 
 function verificarPreenchimento(){
@@ -10,9 +13,27 @@ function verificarPreenchimento(){
         if (item.value.trim() !== '') {
             item.classList.add("preenchido");
             item.classList.remove("naoPreenchido");
+            item.nextElementSibling.classList.remove("mostrar")
         } else {
             item.classList.add("naoPreenchido");
             item.classList.remove("Preenchido");
+            item.nextElementSibling.classList.add("mostrar")
         }
     });
+}
+
+function ajustarTamanhoForm() {
+    // Reseta a altura total a cada chamada da função
+    let totalHeight = 0;
+    const visibleCampos = document.querySelectorAll('.campo-obrigatorio.mostrar');
+
+    visibleCampos.forEach(function(campoVisivel) {
+        totalHeight += campoVisivel.offsetHeight;
+    });
+
+    const novaAltura = formularioSection.offsetHeight + totalHeight;
+
+    if (novaAltura > formularioSection.offsetHeight) {
+        formularioSection.style.height = novaAltura + 'px';
+    }
 }

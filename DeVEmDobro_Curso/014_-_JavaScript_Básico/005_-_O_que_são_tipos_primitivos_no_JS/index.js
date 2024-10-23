5dfb25a1aa6499c7b9312303766de02470809986
mfdzerohour <mfdzerohour@hotmail.com>
2024-10-23 15:51:57 -0400

    Tipos de dados em JavaScript

---
+++ DeVEmDobro_Curso/014_-_JavaScript_Básico/005_-_O_que_são_tipos_primitivos_no_JS/index.js
@@ -0,0 +1,12 @@
+let nome = "Roberto";
+let numero = 1;
+let usuarioVerificado = false;
+
+// if (usuarioVerificado) {
+//     console.log("Usuário verificado");
+// } else {
+//     console.log("Usário não verificado");
+// }
+
+let pessoa = null;
+let indefinido = undefined;

a12f95153ce8a9d0344ce3ff64f18222ccd0ac46
mfdzerohour <mfdzerohour@hotmail.com>
2024-10-24 17:46:08 -0400

    Projetos que desenvolvi, mas ainda irei revisar o conteudo de JS, Jest e ReactJS

---
+++ DeVEmDobro_Curso/014_-_JavaScript_Básico/011_-_O_que_são_e_para_que_servem_condicionais_-_Parte_1/index.js
@@ -0,0 +1,19 @@
+let nomeFilme = "Aquaman";
+
+// if(nomeFilme === "Vingadores"){
+//     console.log("É o filme dos vingadores");
+// }
+
+// if(nomeFilme == "Vingadores"){
+//     console.log("É o filme dos vingadores");
+// } else {
+//     console.log("É outro filme");
+// }
+
+if(nomeFilme == "Vingadores"){
+    console.log("É o filme dos vingadores");
+} else if (nomeFilme === "Batman vs Superman") {
+    console.log("É o filme do Batman vs Superman");
+} else {
+    console.log("É outro filme");
+}
---
+++ DeVEmDobro_Curso/020_-_Desafio_API_Github/src/js/variables.js
@@ -0,0 +1,4 @@
+export { baseUrl, repositoriesQuantity };
+
+const baseUrl = 'https://api.github.com/users';
+const repositoriesQuantity = 10;

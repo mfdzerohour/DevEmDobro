c2e7b08ecc81c412686359471027430572881f0b
mfdzerohour <mfdzerohour@hotmail.com>
2024-11-29 14:54:49 -0400

    Feito pequenas correções, mas "efetivas", mas não estava errado, apenas de forma mais correta de se fazer.

---
+++ DeVEmDobro_Curso/015_-_Exercícios_JavaScript_Básico/015.2.2_-_Exercícios_JavaScript_Básico_-_Exercício_2_Resolucao/index.js
@@ -0,0 +1,8 @@
+const boletoPago = false;
+
+// if (boletoPago === true) { quando é verdadeiro não precisa do true
+if (boletoPago) {
+    alert('O boleto está pago!')
+} else if (boletoPago === false) {
+    alert('O boleto não está pago!')
+}

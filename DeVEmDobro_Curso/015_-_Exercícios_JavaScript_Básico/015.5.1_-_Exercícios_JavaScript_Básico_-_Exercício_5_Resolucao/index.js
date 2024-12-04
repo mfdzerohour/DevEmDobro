abd90b0abe92a9a42a5ae49f9c8f56e9a0907494
mfdzerohour <mfdzerohour@hotmail.com>
2024-12-04 09:14:45 -0400

    Multiplicando o valor por dois, na correção foi mostrado como fazer usando uma variavel, mas nest caso especifico, da para resolver sem, somente coloquei para futuras consultas.

---
+++ DeVEmDobro_Curso/015_-_Exercícios_JavaScript_Básico/015.5.1_-_Exercícios_JavaScript_Básico_-_Exercício_5_Resolucao/index.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 10; i++) {
+    console.log(`O dobro do número ${i} é ${i * 2}`);
+}
+
+//outra forma de resolver colocando uma variavel (dobro)
+// for (let i = 0; i <= 10; i++) {
+//     const dobro = i * 2
+//     console.log(`O dobro do número ${i} é ${dobro}`);
+// }

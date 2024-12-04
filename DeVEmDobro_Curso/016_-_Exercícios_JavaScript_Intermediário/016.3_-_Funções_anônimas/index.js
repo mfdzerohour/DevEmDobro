e6a1cdc07165c0e1be408c9b8a8d555a7bbb7c7b
mfdzerohour <mfdzerohour@hotmail.com>
2024-12-04 16:13:59 -0400

    FUnções anonimas no JavaScript

---
+++ DeVEmDobro_Curso/016_-_Exercícios_JavaScript_Intermediário/016.3_-_Funções_anônimas/index.js
@@ -0,0 +1,13 @@
+// function somar(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = somar(12, 5)
+// console.log(resultadoDaSoma)
+
+let resultadoDaSoma = function(numero1, numero2) {
+    return numero1 + numero2
+}
+
+// console.log(resultadoDaSoma)
+console.log(resultadoDaSoma(12, 5))

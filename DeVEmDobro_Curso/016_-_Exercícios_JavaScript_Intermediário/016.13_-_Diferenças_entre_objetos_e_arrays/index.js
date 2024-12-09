50794adc539dab65a968a66149ec0ccd9d08aa51
mfdzerohour <mfdzerohour@hotmail.com>
2024-12-09 17:53:04 -0400

    Muitas pessoas ficam com dúvidas entre o que é um objeto e o que é um array e quando usar. Nessa aula, vamos nos aprofundar e realizar uma comparação entre os dois tipos.

---
+++ DeVEmDobro_Curso/016_-_Exercícios_JavaScript_Intermediário/016.13_-_Diferenças_entre_objetos_e_arrays/index.js
@@ -0,0 +1,10 @@
+//Arrays é assim que se chama um de frutas
+let frutas = ['uva', 'banana', 'melão']
+
+let videoGame = {
+    nome: 'The Last of Us',
+    genero: 'Acao',
+    desenvolvedor: 'Naughty Dog'
+}
+
+//Começar de novo o video

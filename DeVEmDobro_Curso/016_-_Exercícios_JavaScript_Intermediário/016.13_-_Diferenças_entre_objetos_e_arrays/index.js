b769f312c58f5402ff480fabe71b645d88a069c0
mfdzerohour <mfdzerohour@hotmail.com>
2024-12-10 09:06:03 -0400

    Completando os arrays e objetos de acordo com o modulo da aula.

--- DeVEmDobro_Curso/016_-_Exercícios_JavaScript_Intermediário/016.13_-_Diferenças_entre_objetos_e_arrays/index.js
+++ DeVEmDobro_Curso/016_-_Exercícios_JavaScript_Intermediário/016.13_-_Diferenças_entre_objetos_e_arrays/index.js
@@ -1,10 +1,27 @@
 //Arrays é assim que se chama um de frutas
 let frutas = ['uva', 'banana', 'melão']
 
+//Assim que monta um objetos e suas propriedades
+//objeto
 let videoGame = {
-    nome: 'The Last of Us',
-    genero: 'Acao',
-    desenvolvedor: 'Naughty Dog'
+    //Propiedades
+    nome: 'Xbox',
+    valor: 3000
 }
 
-//Começar de novo o video
+let videoGame2 = {
+    //Propiedades
+    nome: 'Playstation 5',
+    valor: 5000
+}
+
+//Array com objetos dentro
+//videoGames é o array e o que esta dentro é os objetos 
+//(e suas propriedades)
+let videoGames = [videoGame, videoGame2]
+
+//Vendo o que esta dentro do array
+console.log(videoGames)
+
+//Pegando o nome do videoGame[POSICAO_ARRAY].propriedade
+console.log(videoGames[1].nome)

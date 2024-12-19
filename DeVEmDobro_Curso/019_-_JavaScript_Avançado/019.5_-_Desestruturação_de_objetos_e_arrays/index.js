8580b82df0b1fb72abbcfb30303f15dd0e981dd5
mfdzerohour <mfdzerohour@hotmail.com>
2024-12-19 17:32:19 -0400

    A sintaxe de atribuição via desestruturação (destructuring assignment) é uma expressão JavaScript que possibilita extrair dados de arrays ou objetos em variáveis distintas.
    CONTINUAR

---
+++ DeVEmDobro_Curso/019_-_JavaScript_Avançado/019.5_-_Desestruturação_de_objetos_e_arrays/index.js
@@ -0,0 +1,7 @@
+let pessoa = {nome: 'Roberto', Sobrenome: 'Dias',idade: 33};
+// let pessoa = {`nome: ${Roberto}, idade: ${33}`};
+let nome = pessoa.nome;
+let idade = pessoa.idade;
+
+console.assert.console.log(nome);
+console.assert.console.log(idade);

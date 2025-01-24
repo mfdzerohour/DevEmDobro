90ba71db52e039d78730150df83b1506eb8dd115
mfdzerohour <mfdzerohour@hotmail.com>
2025-01-24 14:42:06 -0400

    Nesta aula vai aprender a exportar um modulo, para não ter que reescrever varias vezes a função por exemplo, um modulo financeiro, onde se faz ele e mexe apenas em um local e já funciona importando ele para onde necessita do uso dele no código.

---
+++ DevEmDobro_Curso/019_-_JavaScript_Avançado/019.22_-_Importar_e_exportar_módulos_JavaScript/index.js
@@ -0,0 +1,6 @@
+import {dividir, multiplicar, somar, subtrair} from "./operacoesMatematicas.js";
+
+console.log('Resultado da soma: 1 + 3 = ' + somar(1,3));
+console.log('Resultado da subtracao: 3 - 1 = ' + subtrair(3,1));
+console.log('Resultado da multiplicação: 4 * 3 = ' + multiplicar(4,3));
+console.log('Resultado da Divisão: 6 / 2 = ' + dividir(6,2));
---
+++ DevEmDobro_Curso/019_-_JavaScript_Avançado/019.22_-_Importar_e_exportar_módulos_JavaScript/operacoesMatematicas.js
@@ -0,0 +1,17 @@
+function somar(numero1, numero2){
+    return numero1 + numero2;
+}
+
+function subtrair(numero1, numero2){
+    return numero1 - numero2;
+}
+
+function multiplicar(numero1, numero2){
+    return numero1 * numero2;
+}
+
+function dividir(numero1, numero2){
+    return numero1 / numero2;
+}
+
+export {somar, subtrair, multiplicar, dividir};

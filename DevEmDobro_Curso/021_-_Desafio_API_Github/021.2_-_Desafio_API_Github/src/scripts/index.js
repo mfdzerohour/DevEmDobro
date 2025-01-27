199ec32c930f5f2145848c66d9d3fc136de5d18d
mfdzerohour <mfdzerohour@hotmail.com>
2025-01-27 11:44:27 -0400

    Buscando os repósitorios no github

--- DevEmDobro_Curso/021_-_Desafio_API_Github/021.2_-_Desafio_API_Github/src/scripts/index.js
+++ DevEmDobro_Curso/021_-_Desafio_API_Github/021.2_-_Desafio_API_Github/src/scripts/index.js
@@ -6,9 +6,9 @@ document.getElementById('btn-search').addEventListener('click', () => {
 
 
 //Adiciona o evento keyup ao input
-document.getElementById('input-search').addEventListener('keyup', (e) => {
+document.getElementById('input-search').addEventListener('keypress', (e) => {
     const userName = e.target.value;
-    const key = e.witch || e.keycode;
+    const key = e.which || e.keyCode;
     const isEnterKeyPressed = key === 13;
 
     if(isEnterKeyPressed){
@@ -16,23 +16,54 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     }
 })
 
+//Busca dados do usuário
 async function user(userName){
     const reponse = await fetch (`https://api.github.com/users/${userName}`);
     return await reponse.json();
 }
 
+//Busca repositorios do usuário
+async function repos(userName){
+    const reponse = await fetch (`https://api.github.com/users/${userName}/repos?per_page=10`);
+    return await reponse.json();
+}
+
 function getUserProfile(userName){
+    repos(userName).then(repoData => {
+        console.log(repoData);
+    })
+
     user(userName).then(userData => {
         console.log(userData);
 
-        let userInfo = `<img src="${userData.avatar_url}" alt="Foto do perfil do usuário"/>
-                        <div class="data">
-                            <h1>${userData.name ?? 'Não possui nome cadastrado 😢'}</h1>
-                            <p>${userData.bio ?? 'Não possui bio cadastrada 😢'}</p>
-                            <p>${userData.followers !== null ? `🔎 Segue <strong>${userData.followers}</strong> pessoa(s)` : 'Usuário não segue ninguem 😢'}</p>
-                            <p>${userData.following !== null ? `🕵️‍♂️ Está seguindo <strong>${userData.following}</strong> pessoa(s)` : 'Não esta seguindo ninguem 😢'}</p>
+        let userInfo = `<div class="info">
+                            <img src="${userData.avatar_url}" alt="Foto do perfil do usuário"/>
+                            <div class="data">
+                                <h1>${userData.name ?? 'Não possui nome cadastrado 😢'}</h1>
+                                <p>${userData.bio ?? 'Não possui bio cadastrada 😢'}</p>
+                                <p>${userData.followers !== null ? `🔎 Segue <strong>${userData.followers}</strong> pessoa(s)` : 'Usuário não segue ninguem 😢'}</p>
+                                <p>${userData.following !== null ? `🕵️‍♂️ Está seguindo <strong>${userData.following}</strong> pessoa(s)` : 'Não esta seguindo ninguem 😢'}</p>
+                            </div>
                         </div>`;
 
         document.querySelector('.profile-data').innerHTML = userInfo;
+        getUserRepositories(userName);
+    })
+}
+
+function getUserRepositories(userName){
+    repos(userName).then(reposData => {
+        let repositoriesItens = "";
+        console.log(reposData);
+
+        reposData.forEach(repo => {
+            repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`
+        })
+        document.querySelector('.profile-data').innerHTML += `  
+                                                                <div class="repositories section">
+                                                                    <h2>Repositórios:</h2>
+                                                                    <ul>${repositoriesItens}</ul>
+                                                                </div>`;
+
     })
 }

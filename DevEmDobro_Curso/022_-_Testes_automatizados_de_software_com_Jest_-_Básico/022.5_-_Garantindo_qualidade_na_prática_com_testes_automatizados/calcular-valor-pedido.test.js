383dda7cc86b35b4ae084a8a1a52cf7ed3e34606
mfdzerohour <mfdzerohour@hotmail.com>
2025-01-28 12:23:08 -0400

    Refaturando o código e vendo que ainda vai ter o teste

---
+++ DevEmDobro_Curso/022_-_Testes_automatizados_de_software_com_Jest_-_Básico/022.5_-_Garantindo_qualidade_na_prática_com_testes_automatizados/calcular-valor-pedido.test.js
@@ -0,0 +1,57 @@
+const calcularValorPedido = require("./calcular-valor-pedido");
+
+it("não deve cobrar valor de frete quando valor dos produtos for superior a 500", () => {
+    // AAA - 3 passos de criação de um teste
+
+    // ARRANGE - ARRUMAR - O OBJETO DE TESTE
+    const meuPedido = {
+        itens: [
+                { nome: "Arco encantado", valor: 2000 },
+                { nome: "Entrega", valor: 100, entrega: true },
+        ],
+    };
+
+    // ACT - AÇÃO - O QUE VAI SER TESTADO
+    const resultado = calcularValorPedido(meuPedido)
+
+    // ASSERT - ASSEÇÃO - QUAL RESULTADO ESPERADO
+    expect(resultado).toBe(2000); 
+});
+
+/* ************************************************************************************ */
+
+it("deve cobrar valor de frete quando o valor dos produtos for menor que 500", () => {
+    // ARRANGE
+    const meuPedido = {
+        itens: [
+                { nome: "Sanduíche", valor: 50 },
+                { nome: "Entrega", valor: 100, entrega: true },
+        ],
+    };
+
+    // ACT
+    const resultado = calcularValorPedido(meuPedido);
+    
+    // ASSERT 
+    expect(resultado).toBe(150);
+
+});
+
+/* ************************************************************************************ */
+
+it("Deve cobrar valor de frete caso o valor dos produtos seja EXATAMENTE a 500", () => {
+    // ARRANGE
+    const meuPedido = {
+        itens: [
+                { nome: "Sanduíche", valor: 500 },
+                { nome: "Entrega", valor: 100, entrega: true },
+        ],
+    };
+
+    // ACT
+    const resultado = calcularValorPedido(meuPedido);
+    
+    // ASSERT 
+    expect(resultado).toBe(600);
+
+});

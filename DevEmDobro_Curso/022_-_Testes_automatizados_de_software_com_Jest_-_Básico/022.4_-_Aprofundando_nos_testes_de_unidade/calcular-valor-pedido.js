7f684613924830e89075d9bec0db334e71790270
mfdzerohour <mfdzerohour@hotmail.com>
2025-01-28 12:16:34 -0400

    FAzendo os três testes possiveis

--- DevEmDobro_Curso/022_-_Testes_automatizados_de_software_com_Jest_-_Básico/022.4_-_Aprofundando_nos_testes_de_unidade/calcular-valor-pedido.js
+++ DevEmDobro_Curso/022_-_Testes_automatizados_de_software_com_Jest_-_Básico/022.4_-_Aprofundando_nos_testes_de_unidade/calcular-valor-pedido.js
@@ -1,24 +1,15 @@
-const meuPedido = {
-    itens: [
-                {nome: 'Poçao de vida', valor: 100}, 
-                {nome: 'Espada de prata', valor: 200},
-                {entrega: 'Entrega', valor: 40, entrega: true}
-    ]
-}
+const calcularValorPedido = (pedido) => {
+    const valorProdutos = pedido.itens 
+        .filter((item) => !item.entrega) 
+        .reduce((valorTotal, valorAtual) => valorTotal + valorAtual.valor, 0); 
 
-const calcularValorPedido = pedido => {
-    // pedido.itens.reduce((totalPedido, pedidoAtual) => totalPedido + pedidoAtual.valor, 0)
-    const valorProdutos = pedido.itens
-        .filter(item => !item.entrega)
-        .reduce((totalPedidos, pedidoAtual) => totalPedidos + pedidoAtual.valor, 0)
-    
-    const entrega = pedido.itens.filter(item => item.entrega);
+    const entrega = pedido.itens.filter((item) => item.entrega); 
 
-    if(valorProdutos > 500){
-        return valorProdutos;
+    if (valorProdutos > 500) {
+        return valorProdutos; 
     } else {
         return valorProdutos + entrega[0].valor;
     }
-}
+};
 
-console.log(calcularValorPedido(meuPedido))
+module.exports = calcularValorPedido;

93832a901314aa1e5e0e59739f42ae2f64699a78
mfdzerohour <mfdzerohour@hotmail.com>
2025-01-29 14:13:13 -0400

    O React.JS é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web e não um framework; Biblioteca que serve para a crição de interfaces de usuário; Foi criado em 2013 pelo Facebook e lançado em 2013; É uma biblioteca mais popular para o desenvolvimento web frontend. Usado para criar SPA's (Single Page Applications)

---
+++ DevEmDobro_Curso/023_-_A_Base_do_React/023.7_-_Trabalhando_com_componentes_de_função/vite-projeto-aula/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './components/button/button.jsx'
+
+function App() {
+  return (
+    <>
+      <Button /> <br />
+      <Button /> <br />
+      <Button /> <br />
+      <Button /> <br />
+    </>
+  );
+}
+
+export default App;

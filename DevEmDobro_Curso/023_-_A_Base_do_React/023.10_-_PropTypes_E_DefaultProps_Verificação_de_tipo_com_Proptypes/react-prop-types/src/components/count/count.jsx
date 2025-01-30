72b3b274d18c3ea7b1eaea668b36ffdfff48fed3
mfdzerohour <mfdzerohour@hotmail.com>
2025-01-30 13:59:00 -0400

    Esta aula é um guia completo sobre PropTypes e DefaultProps no ReactJS. Vamos garantir que você tenha as habilidades para verificar os tipos de dados nos seus componentes React.

---
+++ DevEmDobro_Curso/023_-_A_Base_do_React/023.10_-_PropTypes_E_DefaultProps_Verificação_de_tipo_com_Proptypes/react-prop-types/src/components/count/count.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import "./count.css";
+
+const Count = (props) => {
+    return (
+        <>
+            <div className="card">
+                <button onClick={props.incrementCount}>
+                    count is {props.count}
+                </button>
+            </div>
+        </>
+    );
+};
+
+Count.propTypes = {
+    count: PropTypes.number.isRequired,
+    incrementCount: PropTypes.func.isRequired
+};
+
+export default Count;
--- DevEmDobro_Curso/023_-_A_Base_do_React/023.10_-_PropTypes_E_DefaultProps_Verificação_de_tipo_com_Proptypes/vite-projeto-criando-componentes-de-classe/src/components/button/button.jsx
+++
@@ -1,6 +0,0 @@
-import './button.css'
-const Button = () =>{
-    return <button className='btn'>Clique aqui</button>
-}
-
-export default Button

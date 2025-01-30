558a939f352fbdb2262361720603e1c0549d4eea
mfdzerohour <mfdzerohour@hotmail.com>
2025-01-30 14:33:31 -0400

    Prop children
    Nesta aula, você aprenderá sobre uma poderosa técnica chamada "prop children". O prop children permite que você passe componentes, elementos ou até mesmo texto como propriedades para um componente pai no React.
    
    Durante esta aula, você explorará como usar o prop children para criar componentes reutilizáveis e flexíveis. Você entenderá como passar conteúdo personalizado para um componente pai e como acessar e manipular esse conteúdo dentro do componente filho.

---
+++ DevEmDobro_Curso/023_-_A_Base_do_React/023.11_-_Prop_children/vite-projeto-criando-componentes-de-classe/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+// import React from 'react'
+// import Button from "./components/button/button.jsx"
+import Card from "./components/card/card.jsx";
+
+function App() {
+    return (
+        <>
+            <Card>
+                <h3>Titulo card 1</h3>
+                <p>Esse é um texto do card 1</p>
+            </Card>
+            <Card>
+                <h3>Titulo card 2</h3>
+                <p>Esse é um texto do card 2</p>
+            </Card>
+            <Card>
+                <h3>Titulo card 3</h3>
+                <p>Esse é um texto do card 3</p>
+            </Card>
+        </>
+    );
+}
+
+export default App;
---
+++ DevEmDobro_Curso/023_-_A_Base_do_React/023.11_-_Prop_children/vite-projeto-criando-componentes-de-classe/src/components/card/card.jsx
@@ -0,0 +1,34 @@
+// import React from 'react'
+import './card.css'
+
+// class Card extends React.Component{
+//     render(){
+//         return (
+//             <div className="card">
+//                 <h3>{this.props.title}</h3>
+//                 <p>Esse é um texto do card</p>
+//             </div>
+//         )
+//     }
+// }
+
+//Usando funções de classe
+// const card = (props) => {
+//     return (
+//         <div className="card">
+//             <h3>{props.title}</h3>
+//             <p>Esse é um texto do card</p>
+//         </div>
+//     )
+// }
+
+//Usando funções de classe
+const Card = (props) => {
+    return (
+        <div className="card">
+            {props.children}
+        </div>
+    )
+}
+
+export default Card;

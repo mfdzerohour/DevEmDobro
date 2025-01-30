2cbd754f0a4fdde8c252f3464d773294a43b5806
mfdzerohour <mfdzerohour@hotmail.com>
2025-01-30 17:17:41 -0400

    Nesta aula, você aprenderá sobre um conceito importante: listas no React e a prop "key". Listas são elementos essenciais em muitas aplicações React, e entender como trabalhar com elas é fundamental para desenvolver interfaces dinâmicas e eficientes.
    Você aprenderá como criar e renderizar listas no React, utilizando o método map para percorrer e exibir os elementos de um array. Além disso, você será apresentado à propriedade "key", que deve ser atribuída a cada item da lista para ajudar o React a identificar e atualizar corretamente os componentes.

---
+++ DevEmDobro_Curso/023_-_A_Base_do_React/023.14_-_Listas_no_React_e_a_prop_key/vite-projeto-criando-componentes-de-classe/src/components/cards/cards.jsx
@@ -0,0 +1,25 @@
+import Card from "../card/card";
+
+
+const cardsTitle = ['Título card 1', "Título card 2", "Título card 3"]
+const Cards = () => {
+    return (
+        <div>
+            <h2>Meus cards</h2>
+            <div>
+                {
+                    cardsTitle.map((cardTitle, index) => {
+                        return (
+                            <Card key={index}>
+                                <h3>{cardTitle}</h3>
+                                <p>Conteudo card 1</p>
+                            </Card>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Cards;

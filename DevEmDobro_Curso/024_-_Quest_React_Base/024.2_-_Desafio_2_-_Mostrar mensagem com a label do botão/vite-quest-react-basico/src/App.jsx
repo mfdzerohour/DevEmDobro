6206cce3030d58aa871098c117183b17831aa615
mfdzerohour <mfdzerohour@hotmail.com>
2025-01-31 14:05:54 -0400

    Ajustes de arquivo modificados

--- DevEmDobro_Curso/024_-_Quest_React_Base/024.2_-_Desafio_2_-_Mostrar mensagem com a label do botão/vite-quest-react-basico/src/App.jsx
+++ DevEmDobro_Curso/024_-_Quest_React_Base/024.2_-_Desafio_2_-_Mostrar mensagem com a label do botão/vite-quest-react-basico/src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonColor from './component/Button/ButtonColor.jsx'
 import Paragraph from './component/Paragraph/Paragraph.jsx'
 import './App.css'
 
 function App() {
-  const [isMaiusculo, setIsMaiusculo] = React.useState(false);
+  const [isMaiusculo, setIsMaiusculo] = useState(false);
 
   const handleClick = () => {
     setIsMaiusculo(!isMaiusculo);

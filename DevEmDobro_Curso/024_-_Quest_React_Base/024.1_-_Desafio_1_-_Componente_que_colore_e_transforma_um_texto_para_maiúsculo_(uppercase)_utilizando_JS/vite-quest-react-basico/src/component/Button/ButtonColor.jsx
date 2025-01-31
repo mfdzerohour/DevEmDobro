048add02a2fc59d361e50fdc8fd3809f20a55830
mfdzerohour <mfdzerohour@hotmail.com>
2025-01-31 12:31:30 -0400

    Criado o botão, colocado um fundo nele e o texto que será colocado alterado para MAIUSCULA e MUDAR A COR

---
+++ DevEmDobro_Curso/024_-_Quest_React_Base/024.1_-_Desafio_1_-_Componente_que_colore_e_transforma_um_texto_para_maiúsculo_(uppercase)_utilizando_JS/vite-quest-react-basico/src/component/Button/ButtonColor.jsx
@@ -0,0 +1,11 @@
+import "./ButtonColor.css";
+
+function ButtonColor(){
+    return(
+        <>
+            <button className="btn">Mudar cor texto</button>
+        </>
+    )
+}
+
+export default ButtonColor;
---
+++ DevEmDobro_Curso/024_-_Quest_React_Base/024.1_-_Desafio_1_-_Componente_que_colore_e_transforma_um_texto_para_maiúsculo_(uppercase)_utilizando_JS/vite-quest-react-basico/src/component/Paragraph/Paragraph.jsx
@@ -0,0 +1,9 @@
+function Paragraph(){
+    return(
+        <>
+            <p>Você atingirá o sucesso quando apresentar com orgulho as cicatrizes que adquiriu ao longo da sua jornada.</p>
+        </>    
+    )
+}
+
+export default Paragraph;

6eae8ec6829e222da9a0457f9643099441c38858
mfdzerohour <mfdzerohour@hotmail.com>
2025-02-07 10:38:01 -0400

    Nesta aula, você vai descobrir a importância das arrow functions no React. As arrow functions são uma sintaxe especial no JavaScript que trazem benefícios significativos para o desenvolvimento de aplicações React.
    É possivel usar as function no React (nova versão), mas sempre use o Arrow Function, por conseguir ser mais claro e facil de usar.

---
+++ DevEmDobro_Curso/025_-_React_Avançado/025.2_-_Importância_da_arrow_function_no_React/vite-projeto-usando_states_em_um_class_component/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Panel from "./components/panel/panel.jsx";
+
+function App() {
+    return (
+        
+        <>
+            <Panel />
+        </>
+    );
+}
+
+export default App;

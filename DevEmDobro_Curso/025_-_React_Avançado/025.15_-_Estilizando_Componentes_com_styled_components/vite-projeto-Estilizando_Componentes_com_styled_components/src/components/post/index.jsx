43929cc392a635884640aae21be54d9b837b7857
mfdzerohour <mfdzerohour@hotmail.com>
2025-02-24 15:48:21 -0400

    Styled-Components fazendo a aula

---
+++ DevEmDobro_Curso/025_-_React_Avançado/025.15_-_Estilizando_Componentes_com_styled_components/vite-projeto-Estilizando_Componentes_com_styled_components/src/components/post/index.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { styled } from "styled-components";
+
+async function getPost(id) {
+    const response = await fetch(`http://localhost:3000/json/post-${id}.json`)
+    return await response.json()
+}
+
+const PostDetails = () => {
+    const [post, setPost] = useState({})
+
+    const { id } = useParams()
+
+    useEffect(() => {
+        async function fetchData() {
+            const post = await getPost(id)
+            setPost(post.data)
+        }
+
+        fetchData()
+    }, [])
+
+    return (
+        <Section>
+            <Link to='/'>Voltar para os posts</Link>
+                <div>  
+                    <Img src={post.image} alt={post.title} />
+                    <h2>{post.title}</h2>
+                    <p>{post.text}</p>
+                </div>
+        </Section>
+    )
+}
+
+const Section = styled.section `
+    padding: 50px;
+`
+
+const Img = styled.img `
+    width: 100%;
+`
+
+export { PostDetails }

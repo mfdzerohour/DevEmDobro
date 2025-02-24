0b5d574240f88329e43fcd3368124070c2b25521
mfdzerohour <mfdzerohour@hotmail.com>
2025-02-24 13:24:26 -0400

    Rotas do React

---
+++ DevEmDobro_Curso/025_-_React_Avançado/025.13_-_Rotas_no_React/vite-projeto-Rotas_no_React/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { AppRoutes } from './pages/routes.jsx';
+
+function App() {
+    return (
+        <div>
+            <AppRoutes />
+        </div>
+    );
+}
+
+export default App;
---
+++ DevEmDobro_Curso/025_-_React_Avançado/025.13_-_Rotas_no_React/vite-projeto-Rotas_no_React/src/pages/posts.jsx
@@ -0,0 +1,8 @@
+import { PostsList } from '../components/posts'
+const Posts = () => {
+    return (
+        <PostsList />
+    )
+}
+
+export { Posts }

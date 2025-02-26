80470dfd9e3ace87f16e24f98156d88417e6ef89
mfdzerohour <mfdzerohour@hotmail.com>
2025-02-26 12:16:17 -0400

    Criando um custom hook no React pratica
    Esta aula é um mergulho profundo na criação de um custom hook no React. Abordamos passo a passo o processo de desenvolvimento de um hook personalizado, desde a identificação de um caso de uso comum até a implementação e teste. Ideal para desenvolvedores que já têm conhecimento básico em hooks e querem expandir sua caixa de ferramentas de desenvolvimento.

--- DevEmDobro_Curso/026_-_Habilidades_Avançadas/026.2_-_Criando_um_custom_hook_no_React_pratica/vite-project-react-custom-hooks/src/components/SaveButton/SaveButton.jsx
+++ DevEmDobro_Curso/026_-_Habilidades_Avançadas/026.2_-_Criando_um_custom_hook_no_React_pratica/vite-project-react-custom-hooks/src/components/SaveButton/SaveButton.jsx
@@ -1,29 +1,7 @@
-import { useEffect, useState } from "react";
+import { useOnlineStatus } from "../Hooks/useOnlineStatus.jsx";
 
 function SaveButton(){
-    const [ isOnLine, setIsOnLine ] = useState(true);
-
-    useEffect(()=>{
-        function handleOnline(){
-            setIsOnLine(true);
-        }
-
-        function handleOffline() {
-            setIsOnLine(false);
-        }
-
-        window.addEventListener('online', handleOnline);
-        window.addEventListener('offline', handleOffline);
-
-        //Esta instrução é para limpar o componente e não ficar
-        //rodando indenifidamente.
-        return () => {
-            window.removeEventListener("online", handleOnline);
-            window.removeEventListener("offline", handleOffline);
-        }
-
-    }, []);
-
+    const isOnLine = useOnlineStatus();
     function handleSaveClick(){
         console.log("Save Clicked!");
     }
--- DevEmDobro_Curso/026_-_Habilidades_Avançadas/026.2_-_Criando_um_custom_hook_no_React_pratica/vite-project-react-custom-hooks/src/components/StatusBar/StatusBar.jsx
+++ DevEmDobro_Curso/026_-_Habilidades_Avançadas/026.2_-_Criando_um_custom_hook_no_React_pratica/vite-project-react-custom-hooks/src/components/StatusBar/StatusBar.jsx
@@ -1,30 +1,8 @@
-import { useEffect, useState } from "react";
+import { useOnlineStatus } from "../Hooks/useOnlineStatus";
 
 export default function StatusBar(){
-
-    const [ isOnLine, setIsOnLine ] = useState(true);
-
-    useEffect(()=>{
-            function handleOnline(){
-                setIsOnLine(true);
-            }
+    const isOnLine = useOnlineStatus();
     
-            function handleOffline() {
-                setIsOnLine(false);
-            }
-    
-            window.addEventListener('online', handleOnline);
-            window.addEventListener('offline', handleOffline);
-    
-            //Esta instrução é para limpar o componente e não ficar
-            //rodando indenifidamente.
-            return () => {
-                window.removeEventListener("online", handleOnline);
-                window.removeEventListener("offline", handleOffline);
-            }
-    
-        }, []);
-
     return (
         
         <>

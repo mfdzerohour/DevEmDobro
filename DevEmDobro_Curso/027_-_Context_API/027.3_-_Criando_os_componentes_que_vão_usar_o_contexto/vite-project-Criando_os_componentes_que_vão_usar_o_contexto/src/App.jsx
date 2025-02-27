e12879072fb3dedec63fd9704d7181aaaa359637
mfdzerohour <mfdzerohour@hotmail.com>
2025-02-27 09:19:11 -0400

    Criando os arquivos iniciais para a ula 027.3

---
+++ DevEmDobro_Curso/027_-_Context_API/027.3_-_Criando_os_componentes_que_vão_usar_o_contexto/vite-project-Criando_os_componentes_que_vão_usar_o_contexto/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { ThemeProvider } from './contexts/theme-context.jsx'
+
+function App() {
+    return (
+        <>
+            <ThemeProvider>
+
+            </ThemeProvider>
+        </>
+    )
+}
+
+export default App;

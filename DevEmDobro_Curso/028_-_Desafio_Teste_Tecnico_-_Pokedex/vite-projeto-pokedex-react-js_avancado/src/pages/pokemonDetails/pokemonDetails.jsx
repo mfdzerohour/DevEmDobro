119c760325f94fb7bc81bc157b2d4261a9e0cbf5
mfdzerohour <mfdzerohour@hotmail.com>
2025-04-10 17:32:07 -0400

    Alterações

--- DevEmDobro_Curso/028_-_Desafio_Teste_Tecnico_-_Pokedex/vite-projeto-pokedex-react-js_avancado/src/pages/pokemonDetails/pokemonDetails.jsx
+++ DevEmDobro_Curso/028_-_Desafio_Teste_Tecnico_-_Pokedex/vite-projeto-pokedex-react-js_avancado/src/pages/pokemonDetails/pokemonDetails.jsx
@@ -5,10 +5,9 @@ const PokemonDetails = () => {
     const { name } = useParams(); // Obtém o parâmetro "name" da URL
 
     return (
-        <div style={{ padding: "2em" }}>
+        <div>
             <h1>Detalhes do Pokémon</h1>
             <p>Nome: {name}</p>
-            {/* Aqui você pode adicionar mais informações sobre o Pokémon */}
         </div>
     );
 };

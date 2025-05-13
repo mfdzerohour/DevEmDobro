33ec594fdd93a674f7bb7d48370becff4b8ea568
mfdzerohour <mfdzerohour@hotmail.com>
2025-05-13 16:03:51 -0400

    Excluir itens

--- DevEmDobro_Curso/029_-_Redux/05_-_Atualização_para_Redux_Moderno_com_Redux_Toolkit_(RTK)/redux-shop-inicial-main/src/store/actions/cart.js
+++
@@ -1,6 +0,0 @@
-export function addProductsToCart(product) {
-    return {
-        type: "ADD_PRODUCT_TO_CART",
-        payload: product
-    };
-}
--- DevEmDobro_Curso/029_-_Redux/05_-_Atualização_para_Redux_Moderno_com_Redux_Toolkit_(RTK)/redux-shop-inicial-main/src/store/reducers/cart.js
+++
@@ -1,7 +0,0 @@
-export default function reducer(state = [], action) {
-    if (action.type === "ADD_PRODUCT_TO_CART") {
-        return [ ...state, action.payload ];
-    }
-
-    return state;
-}

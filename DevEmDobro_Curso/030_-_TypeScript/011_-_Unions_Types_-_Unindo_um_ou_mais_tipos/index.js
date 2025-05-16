0072cda11237087f5054d98679dfad2e18758cdc
mfdzerohour <mfdzerohour@hotmail.com>
2025-05-16 09:36:47 -0400

    Usando o UNION que pode ser unidos quando colocado | por exemplo (id: number | string) vai rceber tanto NUMBER quanto STRING

--- DevEmDobro_Curso/030_-_TypeScript/011_-_Unions_Types_-_Unindo_um_ou_mais_tipos/index.js
+++ DevEmDobro_Curso/030_-_TypeScript/011_-_Unions_Types_-_Unindo_um_ou_mais_tipos/index.js
@@ -1,13 +1,9 @@
 // TypeScript - Unions: unindo um ou mais tipos
 // Nessa aula vamos te apresentar o conceito do Union types, com isso a temos a possibilidade de unir dois ou mais tipos.
-
 //Quando usa a | ele esta unindo um ou outro (id: number | string)
-
-function printClienteID(id: number | string) {
-    console.log(`O ID do cliente é: ${id}`);
+function printClienteID(id) {
+    console.log("O ID do cliente \u00E9: ".concat(id));
 }
-
 printClienteID(1);
 printClienteID('1');
 printClienteID('sadasdsds');
-

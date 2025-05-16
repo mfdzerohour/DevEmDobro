3b3da6dde6b0f3556e79da635555ab968046e204
mfdzerohour <mfdzerohour@hotmail.com>
2025-05-16 09:31:35 -0400

    Alterações

---
+++ DevEmDobro_Curso/030_-_TypeScript/011_-_Unions_Types_-_Unindo_um_ou_mais_tipos/index.ts
@@ -0,0 +1,9 @@
+// TypeScript - Unions: unindo um ou mais tipos
+// Nessa aula vamos te apresentar o conceito do Union types, com isso a temos a possibilidade de unir dois ou mais tipos.
+
+function printClienteID(id: number) {
+    console.log(`O ID do cliente é: ${id}`);
+}
+
+printClienteID(1);
+
